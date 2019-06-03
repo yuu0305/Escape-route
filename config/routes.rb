1e5f76feabbca9e401768e6ac053716d66f74c80
yuu0305 <segyayuu8@gmail.com>
2019-06-03 17:06:09 +0900

    Edit  notifies/create
    ビューを作るため一旦仮置きのルーティングをgetで追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'talks#top'
+
   resources :talks
   resources :notifies, only: [:new ,:create]
+  get 'notifies/create' => 'notifies#create'
+  # 仮置きのcreate、一旦ビューを作るため追加
 end
