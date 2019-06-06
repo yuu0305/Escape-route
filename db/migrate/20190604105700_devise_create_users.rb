8a05f5a17ccbc2c0e4804cb0916262d02461a8a0
yuu0305 <segyayuu8@gmail.com>
2019-06-06 16:36:50 +0900

    add_column
    カラムの追加
    ストロングパタメーター編集
    データベース保存　確認

--- db/migrate/20190604105700_devise_create_users.rb
+++ db/migrate/20190604105700_devise_create_users.rb
@@ -6,7 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :name,null: false
+      t.string :furigana,null: false
+      t.string :school,null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
