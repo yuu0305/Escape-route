d12de6312327e61d4a92c24be425e527af12a585
yuu0305 <segyayuu8@gmail.com>
2019-06-18 13:27:20 +0900

    User has one room (Conditional branching )
    user_roomのuser_idにcurrent_user.idがあるかどうかを見て、なければnew、あれば
    既存のuser_roomにリダイレクト

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -5,9 +5,9 @@ class TalksController < ApplicationController
   def index
     # binding.pry
     @talk = Talk.new
-    @talks = Talk.where(user_room_id: params[:users_room_id])
-    @right_talks = Talk.where(user_id: current_user.id).order("created_at ASC")
-    @left_talks = Talk.where.not(user_id: current_user.id).order("created_at ASC")
+    @talks = Talk.where(users_room_id: params[:users_room_id])
+    # @right_talks = Talk.where(user_id: current_user.id).order("created_at ASC")
+    # # @left_talks = Talk.where.not(user_id: current_user.id).order("created_at ASC")
     # binding.pry
     @time = Time.now
   end
--- app/controllers/users_rooms_controller.rb
+++ app/controllers/users_rooms_controller.rb
@@ -1,19 +1,31 @@
 class UsersRoomsController < ApplicationController
 # before_action :authenticate_user!
 
-    def index
-    end
+  def index
+  end
 
 
-    def create
+  def create
+    unless @user = User.find(current_user.id).users_room.present?
       @room = UsersRoom.new(user_id: current_user.id)
       @room.save!
-      # binding.pry
       redirect_to "/users_rooms/#{@room.id}/talks"
+    else
+      @already = User.find(current_user.id)
+      redirect_to "/users_rooms/#{@already.users_room.id}/talks"
     end
+  end
 
-    # private
-    # def users_room_params
-    #   params.require(:usersroom).merge(user_id: current_user.id)
-    # end
+  # private
+  # def users_room_params
+  #   params.require(:usersroom).merge(user_id: current_user.id)
+  # end
 end
+
+
+
+
+
+# @room = UsersRoom.new(user_id: current_user.id)
+# @room.save!
+# redirect_to "/users_rooms/#{@room.id}/talks"
